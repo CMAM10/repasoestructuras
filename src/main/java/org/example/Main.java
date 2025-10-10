a7eb3dad81804e48161e75ce8f40e86d878a722f
Alvaro Perez <juan@gmail.com>
2025-10-10 17:46:05 -0500

    feat se crea inicio de sesion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,41 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        String usuarioRegistradoBD="user024";
+        String contraseñaRegistradaBD="admin123";
+        String usuarioDigitado;
+        String contarseñaDigitada;
+
+        Scanner leerTeclado=new Scanner(System.in);
+        Integer contador=0;
+        String colorAzul="\u001B[34m";
+        String colorVerde="\u001B[32m";
+        String colorRojo="\u001B[31m";
+
+        System.out.println(colorAzul+"\nBienvenido al sistema de control SAS: 🤖");
+
+        while(contador<3){
+
+            System.out.print(colorAzul+"\nDigita tu nombre de usuario: ");
+            usuarioDigitado=leerTeclado.nextLine();
+
+            System.out.print(colorAzul+"Digita tu contarseña: ");
+            contarseñaDigitada=leerTeclado.nextLine();
+
+            if(usuarioDigitado.equals(usuarioRegistradoBD) && contarseñaDigitada.equals(contraseñaRegistradaBD)){
+                System.out.println(colorVerde+"\nBienvenido 🚀");
+            }else{
+                System.out.println(colorRojo+"\nRevisa los datos ingresados 💩");
+                contador++;
+            }
+
+
+
+        }
 
     }
 }
