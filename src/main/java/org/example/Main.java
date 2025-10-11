605b880779b748b7f159a0d85bea38beceaa7430
Alvaro Perez <juan@gmail.com>
2025-10-10 20:45:36 -0500

    ok

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -28,6 +31,59 @@ public class Main {
 
             if(usuarioDigitado.equals(usuarioRegistradoBD) && contarseñaDigitada.equals(contraseñaRegistradaBD)){
                 System.out.println(colorVerde+"\nBienvenido 🚀");
+
+                System.out.println("\nMenu de opciones");
+                System.out.println("****************");
+                System.out.println("1. Registrar Productos 📝");
+                System.out.println("2. Revisar Inventario 👀");
+                System.out.println("3. Modificar Producto 📲");
+                System.out.println("4. Eliminar Producto ✖️");
+                System.out.println("Digita 5 para salir 🚪");
+
+                Integer opcionSeleccionada=1;
+                HashMap<String,Object> producto=new HashMap<>();
+                while(opcionSeleccionada != 5){
+                    System.out.print("\n Que opciones eliges? ");
+                    opcionSeleccionada=leerTeclado.nextInt();
+
+                    if(opcionSeleccionada==1){
+                        System.out.println("Ingresando un producto");
+                        //Un producto es un diccionario
+                        //Declarando un diccionario:
+
+                        //Poblando (llenando) un diccionario
+                        producto.put("id",5);
+                        producto.put("nombre","Play Station 5");
+                        producto.put("disponible",true);
+                        producto.put("precioUnitario",500.5);
+
+                        //Necesito llevar el diccionario a una lista
+                        //Declarando una lista en java
+                        ArrayList<Object>productos=new ArrayList<>();
+                        // Poblando o ingersando un elemento a una lista
+                        productos.add(producto);
+                        System.out.println(productos);
+
+
+
+                    }else if(opcionSeleccionada==2){
+                        System.out.println("Mostrando el inventario");
+                    }else if(opcionSeleccionada==3){
+                        System.out.println("modificando un producto");
+                    }else if(opcionSeleccionada==4){
+                        System.out.println("retirando un producto");
+                    }else if(opcionSeleccionada==5){
+                        System.out.println("Gracias");
+                        break;
+                    }else{
+                        System.out.println("La opcion ingresada no es valida");
+                    }
+                }
+                break;
+
+
+
+
             }else{
                 System.out.println(colorRojo+"\nRevisa los datos ingresados 💩");
                 contador++;
